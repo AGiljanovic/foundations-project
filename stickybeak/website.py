784c6fc1adb63f7b5f2688ad3398bc6edcc0af45
“[antea <antea.giljanovic@code.berlin>
2021-05-08 22:57:33 +0200

    sql fixing

--- stickybeak/website.py
+++ stickybeak/website.py
@@ -1,11 +1,43 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for, jsonify
 from db import add_emails
+from flask import jsonify
+import os
+import pymysql
 
 app = Flask(__name__)
 
 # configure Flask using environment variables
 app.config.from_pyfile("config.py")
+
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    try:
+        if os.environ.get('GAE_ENV') == 'standard':
+            conn = pymysql.connect(user=db_user, password=db_password,
+                                   unix_socket=unix_socket, db=db_name,
+                                   cursorclass=pymysql.cursors.DictCursor
+                                   )
+    except pymysql.MySQLError as e:
+        print(e)
+
+    return conn
+
+
+def add_emails(email):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO emails (emails) VALUES(%s)',
+                       (email["email"]))
+    conn.commit()
+    conn.close()
 
 
 @app.route('/')
